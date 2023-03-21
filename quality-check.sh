88fceef686a11a8eb0b12150406acb791f9b15d2
Mariamts <45490301+Mariamts@users.noreply.github.com>
2023-03-21 18:49:28 +0400

    Update quality-check.sh

--- quality-check.sh
+++ quality-check.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "Navigating to the shop/angular/cloudfront directory"
+echo "Navigating to the shop/angular/cloudfront directory..."
 cd shop/angular/cloudfront
 
-echo "Running linting"
+echo "Running linting..."
 npm run lint
 
-echo "Running tests"
+echo "Running tests..."
 npm run test
 
-echo "Auditing npm packages"
+echo "Auditing npm packages..."
 npm audit
 
 if [ "$(npm audit --json | jq '.metadata.vulnerabilities')" != "{}" ]; then
-	echo "Vulnerabilities found"
+	echo "Vulnerabilities found."
 else 
-	echo "No vulnerabilities found"
+	echo "No vulnerabilities found."
 fi
