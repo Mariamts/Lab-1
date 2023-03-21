46091fcd3de2443a970d2001465088cc1ab5afb4
Mariamts <45490301+Mariamts@users.noreply.github.com>
2023-03-21 18:32:53 +0400

    Update build-client.sh
    add info logs for steps

--- build-client.sh
+++ build-client.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
+echo "Cloning repository..."
 # clone repository
 git clone https://github.com/EPAM-JS-Competency-center/shop-angular-cloudfront.git
 
 # navigate
 cd shop-angular-cloudfront
 
+echo "Installing npm dependencies..."
 # install npm dependencies
-
 npm install
 
 # set environment variable
 ENV_CONFIGURATION=production
 
+echo "Building client app with specified configuration: $ENV_CONFIGURATION ..."
 # build the client app with the specified configuration
 npm run build -- --configuration $ENV_CONFIGURATION
 
+echo "Removing existing zip file..."
 # remove the existing zip file
-
-if [-f ../dist/client-app.zip]; then 
+if [ -f ../dist/client-app.zip ]; then 
 	rm ../dist/client-app.zip
 fi
 
+echo "Compressing built content/files into a zip file..."
 # compress the built content/files into a zip file
 zip -r ../dist/client-app.zip dist/*
